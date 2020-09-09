84828f83cd0ffdaa6ffda9ec17588442e4273ab1
Ray Mitchell <ray.mitchell@accenture.com>
2020-09-08 21:00:27 -0700

    add corporate training materials

---
+++ test-driven-development/src/test-driven-development/Chapter1/StringUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChapterOne.Utilities
+{
+    public class StringUtilities
+    {
+        public int CountOccurences(string stringToCheck, string stringToFind)
+        {
+            return 1;
+        }
+    }
+}
